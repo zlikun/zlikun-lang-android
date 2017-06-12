ce26465842636f75f7f80f0edd07faf50a6de3c6
zlikun <zlikun-dev@hotmail.com>
2017-06-12 22:54:12 +0800

    Activity生命周期

--- activity/src/main/java/com/zlikun/lang/MainActivity.java
+++ activity/src/main/java/com/zlikun/lang/MainActivity.java
@@ -6,6 +6,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+/**
+ * Activity 生命周期
+ * https://developer.android.google.cn/guide/components/activities.html
+ * https://developer.android.google.cn/images/activity_lifecycle.png
+ *
+ * 第1次打开
+     06-12 14:38:38.683 5104-5104/? I/System.out: onCreate ...
+     06-12 14:38:38.683 5104-5104/? I/System.out: onStart ...
+     06-12 14:38:38.684 5104-5104/? I/System.out: onResume ...
+ * 按Home键
+     06-12 14:39:05.987 5104-5104/com.zlikun.lang I/System.out: onPause ...
+     06-12 14:39:06.073 5104-5104/com.zlikun.lang I/System.out: onStop ...
+ * 按菜单键
+     06-12 14:39:20.635 5104-5104/com.zlikun.lang I/System.out: onRestart ...
+     06-12 14:39:20.635 5104-5104/com.zlikun.lang I/System.out: onStart ...
+     06-12 14:39:20.635 5104-5104/com.zlikun.lang I/System.out: onResume ...
+ * 按返回键
+     06-12 14:39:41.899 5104-5104/com.zlikun.lang I/System.out: onPause ...
+     06-12 14:39:42.614 5104-5104/com.zlikun.lang I/System.out: onStop ...
+     06-12 14:39:42.614 5104-5104/com.zlikun.lang I/System.out: onDestroy ...
+ * 按菜单键
+     06-12 14:44:21.061 5104-5104/com.zlikun.lang I/System.out: onCreate ...
+     06-12 14:44:21.061 5104-5104/com.zlikun.lang I/System.out: onStart ...
+     06-12 14:44:21.062 5104-5104/com.zlikun.lang I/System.out: onResume ...
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // 设置控件布局
         setContentView(R.layout.activity_main);
+
+        System.out.println("onCreate ...");
 
         // findViewById() 所有的可视化控件都可以用该方法获取(可能需要强转类型)
         // setOnClickListener() 用于设置点击事件
@@ -26,5 +53,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart ...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume ...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause ...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("onStop ...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("onDestroy ...");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("onRestart ...");
     }
 }
