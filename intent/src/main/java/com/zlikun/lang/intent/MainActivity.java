3ab0b1dfd473c2e3991fea67d8102cac6ca65a67
zlikun <zlikun-dev@hotmail.com>
2017-06-15 23:01:07 +0800

    处理要打开的Activity不存在或无法打开异常

--- intent/src/main/java/com/zlikun/lang/intent/MainActivity.java
+++ intent/src/main/java/com/zlikun/lang/intent/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
                 // 使用纯字符串的一个好处是，可以启动其它应的Activity
                 // com.zlikun.lang.intent.action.HelloActivity 是app模块里的Activity
                 // 注意，两个模块包名不能相同，否则无法同时安装在同一个模拟器上
-                // startActivity(new Intent("com.zlikun.lang.intent.action.HelloActivity"));
+
+//                try {
+//                    startActivity(new Intent("com.zlikun.lang.intent.action.HelloActivity"));
+//                } catch (Exception e) {
+//                    // 处理要打开的Activity不存在或无法打开异常
+//                    Toast.makeText(MainActivity.this ,"无法启动指定的Activity!" ,Toast.LENGTH_SHORT) ;
+//                }
 
             }
         });
